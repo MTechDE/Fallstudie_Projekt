f9dfdffe963699c93a25ab5803c76c410626c3ec
Daniel Sogl <mytechde@outlook.com>
2016-10-05 12:56:47 +0200

    Renamed Main Klass

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -3,11 +3,11 @@ package UI;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class MainUICtrl extends Application {
+public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+		// TODO FXML Datei einbinden!
 	}
 
 	public static void main(String[] args) {
