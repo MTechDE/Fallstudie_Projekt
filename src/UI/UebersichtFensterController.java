4525b47e335ad27ba87837dd3f0d3665e657b1b8
Patrick <akrilos@web.de>
2016-10-17 13:55:05 +0200

    Aktualisierung

--- src/UI/UebersichtFensterController.java
+++ src/UI/UebersichtFensterController.java
@@ -1,15 +1,10 @@
 package UI;
 
 import javafx.fxml.FXML;
-
 import javafx.scene.control.Button;
-
-import javafx.scene.control.Label;
-
-import javafx.scene.control.Tab;
-
 import javafx.scene.control.CheckBox;
-
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 
 public class UebersichtFensterController {
