415a8df129391270ad66b818ad6f648bc3ba3a56
Daniel Sogl <mytechde@outlook.com>
2016-10-31 15:54:14 +0100

    Erweiterte Übersicht
    Quartale werden nun richtig berechnet

--- src/export/Excel.java
+++ src/export/Excel.java
@@ -1,26 +1,21 @@
 package export;
 
 import java.io.FileOutputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import projektDaten.Kompetenz;
 import projektDaten.Phase;
 import projektDaten.Projekt;
 
 /**
- * Exportiert Projekte in eine CSV oder Text Datei
+ * Die Excel Klasse exportiert das Projekt mit Hilfe der Apache POI Bibliothek in einem
+ * xlsx Format an den gewählten Speicherort.
  * 
- * @author mytec
+ * @author Daniel Sogl
  *
  */
 public class Excel {
@@ -138,7 +133,7 @@ public class Excel {
 			}
 		}
 
-		// Erstelle erweiterte Ansicht
+		// Erstelle Erweiterte-Ansicht
 
 		// Erzeuge Header
 
@@ -148,9 +143,9 @@ public class Excel {
 		projekt.getPhasen().sort(Comparator.comparing(Phase::getEndDate));
 		int endJahr = Integer
 				.parseInt(projekt.getPhasen().get(projekt.getPhasen().size() - 1).getEndDate().substring(0, 4));
-		int anzJahre = endJahr - startJahr;
-		if(anzJahre == 0)
-			anzJahre = 1;
+		
+		// Es muss mindestens ein Jahr geben
+		int anzJahre = (endJahr - startJahr) + 1;
 		
 		// Erzeuge Kompetenzen Spalte
 		for (int i = 0; i < projekt.getKompetenzen().size(); i++) {
@@ -161,28 +156,51 @@ public class Excel {
 
 		// Erzeuge Quartal Spalten
 		header1 = sheet2.createRow(3);
+		header1.createCell(0).setCellValue("Technologien / Kompetenzen");
 		for (int j = 0; j < anzJahre; j++) {
 			for(int i = 1; i <= 4; i++){
 				header1.createCell((j * 4) + i).setCellValue("Q" + i + " - " + String.valueOf(startJahr + j));
 			}			
 		}
+	
 		
-		// Erzeuge Phasen Header
-		header1 = sheet2.createRow(4);
-		header1.createCell(0).setCellValue("Technologien / Kompetenzen");
+		// Weise die PT dem richtigen Quartal sowie der richtigen Kompetenz zu
 		
-		// Weise Phasen den richtigen Quartalen zu
+		// Durchlaufe jedes Jahr und dessen Quartale um die passenden PT herauszufiltern
 		for (int j = 0; j < anzJahre; j++) {
+			// 4 Quartale pro Jahr
 			for(int i = 1; i <= 4; i++){
-				System.out.println("Jahr: " + (startJahr + j) + " Quartal: " + i);
-			}
+				
+				for (int p = 0; p < phasen.size(); p++) {
+					for (int k = 0; k < kompetenzen.size(); k++) {
+						double gesPT = 0;
+						for (int a = 0; a < phasen.get(p).getAufwände().size(); a++) {
+							if (phasen.get(p).getAufwände().get(a).getZugehoerigkeit().equals(kompetenzen.get(k).getName()) && (startJahr + j) == Integer.parseInt(phasen.get(p).getStartDate().substring(0, 4))) {
+								
+								// Berechne anzahl der Jahre zwischen Start und Enddatum
+								int difJahre = Integer.parseInt(phasen.get(p).getEndDate().substring(0, 4)) - Integer.parseInt(phasen.get(p).getStartDate().substring(0, 4));
+								
+								// Berechne die Anzahl der Quartale durch die die Phase geht
+								
+								int startMonat = Integer.parseInt(phasen.get(p).getStartDate().substring(5, 7));
+								int endMonat = Integer.parseInt(phasen.get(p).getEndDate().substring(5, 7));
+								
+								// Ist die Differenz 0, sind sie im selben Monat
+								
+								System.out.println("Differezn Start und Enddatum: " + (endMonat - startMonat));
+								
+							}
+						}
+						int row = 10 + kompetenzen.size() + k;
+						int cell = 1 + p;
+
+//						sheet1.getRow(row).createCell(cell).setCellValue(gesPT);
+
+					}
+				}
+				
+			}			
 		}
-		
-		for (int i = 0; i < phasen.size(); i++) {
-			header1.createCell(i + 1).setCellValue(phasen.get(i).getName());
-		}
-		
-		// Erzeuge Phasen Überschriften
 		
 
 		// Schreibe die Excel Datei in den angegebenen Pfad
